217576a88ddc07627924954c91ed4cbe8115b56f
rawfour <rafaldziedziela@outlook.com>
2020-02-23 21:13:28 +0100

    Updates

---
+++ precache-manifest.0bf8c093de05fcc8c6abed082311858a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b26b294fa6bcf84088b25d86620464",
+    "url": "/shopping-cart-react/index.html"
+  },
+  {
+    "revision": "b90ce434d7ba08b75d46",
+    "url": "/shopping-cart-react/static/js/2.eed0252f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/shopping-cart-react/static/js/2.eed0252f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7959592dae8be1082c19",
+    "url": "/shopping-cart-react/static/js/main.47ed9263.chunk.js"
+  },
+  {
+    "revision": "6735a015b5d70f216fec",
+    "url": "/shopping-cart-react/static/js/runtime-main.9110f075.js"
+  },
+  {
+    "revision": "ea4d4547ed17367a8f9bf099c93b7716",
+    "url": "/shopping-cart-react/static/media/close.ea4d4547.svg"
+  },
+  {
+    "revision": "67b8cf8c6d2da998edb7622430aa3f84",
+    "url": "/shopping-cart-react/static/media/remove.67b8cf8c.svg"
+  }
+]);
